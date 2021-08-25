e3552d176b26f23397963e404853f2a3e52b2ff5
Matías Gutiérrez <75875477+1M4TY@users.noreply.github.com>
2021-08-24 21:33:58 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#PRUEBA DE MINIJUEGO
+#19 de agosto de 2021 inicio
+input("Hola, soy steve")
+print()
+input("te guiare por este pequeño tutorial")
+print()
+input("aunque solo soy una IA diseñada para satisfacer tu ganas de jugar videojuegos.")
+print()
+input("Ahora debes elegir entre una de estas opciones:" )
+print()
+while bucle == "Si" or "si"
+  print (" (1) Obtener 10 monedas")
+  print (" (2) Robar 100 monedas")
+  print()
+  op_monedas = int(input("¿Que eliges? "))
+  print()
+  if op_monedas == 1 :
+      print ("Aaawww, que lindo quiere que le regale 10 monedas, pero eso no sera tan facil deberas decirme algo pequeño y sencillo")
+      res_capital = input ("\n\t¿Cual es la capital de China?\n")
+      if str(res_capital) == "pekin":
+        print ("\nMuy bien, Usted sabe bastante :) ...")
+      else:
+        print ("\nUsted no es muy inteligente al parecer :( ...")
+  else:
+    if op_monedas == 2 :
+      print ("Aaaah, eres un ladronsuelo algo astuto, pero  la ves imbecil, para robarle a la persona que te da una opción de continuar con tu vida, aun que es algo ficticio, ¿pero acaso no lo has hecho en la vida real?")
+      
